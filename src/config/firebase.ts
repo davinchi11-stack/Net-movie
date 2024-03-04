9d9b8714287615302f0faccb32cb05659e5debf9
Divinewill <108321179+davinchi11-stack@users.noreply.github.com>
2024-03-04 18:32:13 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,15 +7,7 @@ import { getAuth , GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDBP1s-rbVyjRs2RyyIv-Id4sE3u4xzmcs",
-  authDomain: "netmovies-6d172.firebaseapp.com",
-  projectId: "netmovies-6d172",
-  storageBucket: "netmovies-6d172.appspot.com",
-  messagingSenderId: "899257357293",
-  appId: "1:899257357293:web:27b9baf41c5b4c3fc5cde4",
-  measurementId: "G-T0TPTDRYT7"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
